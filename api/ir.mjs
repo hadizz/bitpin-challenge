36f95c876967420b94034f6b43676a1097a0c626
Hadi Zare <hadizareoriginal@gmail.com>
2024-11-13 13:17:15 +0330

    vercel edge try 1

--- api/ir.mjs
+++ api/ir.mjs
@@ -2,9 +2,5 @@ export async function GET(request) {
   const url = new URL(request.url);
   const targetUrl = `https://api.bitpin.ir${url.pathname.replace('/api/ir', '')}`;
 
-  return fetch(targetUrl, {
-    method: request.method,
-    headers: request.headers,
-    body: request.body,
-  });
+  return { request: JSON.stringify(request), url: url.toJSON(), targetUrl };
 }
