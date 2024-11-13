1bc001d7a3f71f396fbea47658724632b21237c1
Hadi Zare <hadizareoriginal@gmail.com>
2024-11-14 00:33:32 +0330

    edge add matches

---
+++ api/matches.mjs
@@ -0,0 +1,18 @@
+async function getBitpinApiUrl(apiUrl) {
+  const res = await fetch(apiUrl);
+  return res.json();
+}
+
+export async function GET(request) {
+  const url = new URL(request.url);
+  const id = url.searchParams.get('id');
+  const targetUrl = `https://api.bitpin.org/v1/mth/matches/${id}/`;
+
+  const data = await getBitpinApiUrl(targetUrl);
+
+  return new Response(JSON.stringify({ data }), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
