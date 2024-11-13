a6fe308c09a85d882d3208282681430fea6899f4
Hadi Zare <hadizareoriginal@gmail.com>
2024-11-13 11:08:05 +0330

    fix: reverse proxy

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,8 +2,8 @@ const isDevelopment = import.meta.env.MODE === 'development';
 console.log('isDevelopment', isDevelopment);
 
 const config = {
-  baseUrlIr: isDevelopment ? import.meta.env.VITE_BASE_URL_IR : '/api',
-  baseUrlOrg: isDevelopment ? import.meta.env.VITE_BASE_URL_ORG : '/api-org',
+  baseUrlIr: isDevelopment ? import.meta.env.VITE_BASE_URL_IR : '/api/',
+  baseUrlOrg: isDevelopment ? import.meta.env.VITE_BASE_URL_ORG : '/api-org/',
 };
 
 export default config;
